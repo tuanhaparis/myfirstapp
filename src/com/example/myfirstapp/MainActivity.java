c9eaae40a6b0c334bf467f5cd50ea21e6a6fff7a
Nguyen Thanh Tuan <tuan_nt@septeni-technology.jp>
2014-10-17 16:33:36 +0700

    push new update

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
@@ -24,8 +25,9 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    	MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -34,10 +36,17 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+        	case R.id.action_search:
+        		openSearch();
+        		return true;
+        	case R.id.action_settings:
+        		openSetting();
+        		return true;
+        	default:
+        		return super.onOptionsItemSelected(item);
+
         }
-        return super.onOptionsItemSelected(item);
     }
     /** Called when the user clicks the Send button */
     public void sendMessage(View view){
